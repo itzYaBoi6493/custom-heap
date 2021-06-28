1b4722cdfd2bb2d66f72c0a16c616f94de880851
Akash Sooraj <akashsooraj6493@gmail.com>
2021-06-28 20:29:38 +0530

    add test client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod pqueue;
+
+use std::cmp::Ordering;
+use pqueue::Heap;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut h: Heap<i32> = Heap::new(|i, j| {
+        if *i < *j {
+            Ordering::Greater
+        } else if *i == *j {
+            Ordering::Equal
+        } else {
+            Ordering::Less
+        }
+    });
+
+    for i in 1..=10 {
+        h.insert(i);
+    }
+
+    while !h.is_empty() {
+        print!("{} ", h.extract_min().unwrap());
+    }
+    println!();
+
+    println!("heap: {:?}", h.get_size());
 }
